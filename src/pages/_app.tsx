75a7fd6fbafb2aec6dc3595fbab9c7c2dea4ec17
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-02-06 15:18:25 -0300

    Add SEO info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,17 +8,13 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Learn Japanese</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/icon.png" />
+        <link rel="apple-touch-icon" href="/img/icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Zhi+Mang+Xing&display=swap"
           rel="stylesheet"
-        />
-        <meta
-          name="description"
-          content="Learn hiragana and katakana in an easy way."
         />
       </Head>
       <GlobalStyles />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
 import Main from 'components/Main'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
-  return <Main />
+  return (
+    <>
+      <NextSeo
+        title="Learn Japanese"
+        description="Learn hiragana and katakana in an easy way."
+        canonical="https://www.learnjapanese.vercel.app"
+        openGraph={{
+          url: 'https://www.learnjapanese.vercel.app',
+          title: 'Learn Japanese',
+          description: 'Learn hiragana and katakana in an easy way.',
+          images: [
+            {
+              url: 'https://www.learnjapanese.vercel.app/img/cover.png'
+            }
+          ],
+          site_name: 'LearnJapanese'
+        }}
+        twitter={{
+          handle: '@Willian_justen',
+          site: '@Willian_justen',
+          cardType: 'summary_large_image'
+        }}
+      />
+      <Main />
+    </>
+  )
 }
